0a4481a55056da1505d6eb02fe8985d65b21306d
Krystian Shanahan <shanahan.k@etoncollege.org.uk>
2022-03-03 12:09:20 +0000

    Initial Commit

---
+++ DataStructuresVisualiser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DataStructuresVisualiser
+//
+//  Created by Shanahan, Krystian (JD) on 03/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataStructuresVisualiser/DataStructuresVisualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresVisualiserApp.swift
+//  DataStructuresVisualiser
+//
+//  Created by Shanahan, Krystian (JD) on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresVisualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
